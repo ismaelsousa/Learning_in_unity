5b9c410e74b78cc232afbab82f7cb595eeabbecb
ismaelsousa <ismaelsousa1998@outlook.com>
2019-03-20 17:03:08 -0300

    Missil Concluído
    Este projeto é um missíl que segue um alien

---
+++ Missil/Assets/Script/HomingMissile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HomingMissile : MonoBehaviour
+{
+
+    public Transform target;//alvo
+    private Rigidbody2D rb;
+    public int speed = 5;
+	public int rotateSpeed = 200;
+	public GameObject explosionEffect;
+   
+   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate(){
+		Vector2 direction = (Vector2)target.position - rb.position;
+		direction.Normalize();
+		float rotateAmount = Vector3.Cross(direction, transform.up).z;
+		rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.up *speed;
+    }
+	
+	void OnTriggerEnter2D(){
+		Instantiate(explosionEffect, transform.position, transform.rotation);
+		Destroy(gameObject);
+		
+	}
+}
